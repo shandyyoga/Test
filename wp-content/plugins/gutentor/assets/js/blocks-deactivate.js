549dac808332f137ca2816003a52e0d4ffe676ea
danyyudha <danyyudha75@gmail.com>
2020-09-13 02:46:39 +0700

    Revert "Initial Commit"
    This reverts commit 840c1a1896b0f264ce8d50cdf7a17b43b37c79be.

--- wp-content/plugins/gutentor/assets/js/blocks-deactivate.js
+++
@@ -1,9 +0,0 @@
-let blocksStatus = GUTENTOR_BLOCKS.status;
-
-if( typeof wp.blocks.unregisterBlockType !== "undefined" ){
-	Object.keys( blocksStatus ).map( function( key ){
-		if( blocksStatus[ key ] === 'disabled' ){
-			wp.blocks.unregisterBlockType( 'gutentor/' + key );
-		}
-	});
-}
